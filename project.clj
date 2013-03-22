a8e588840ccda5ead106780113bc8ef3fe4369a5
Marko Topolnik <mtopolnik@inge-mark.hr>
2013-03-22 19:08:31 +0100

    Move to 2.1.1-SNAPSHOT (by lein-release)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ingemark/lein-release "2.1.0"
+(defproject com.ingemark/lein-release "2.1.1-SNAPSHOT"
   :description "Cut a new release: deploy, tag, bump to new snapshot version."
   :url "https://github.com/Inge-mark/lein-release"
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
